1b7706cacbc5d9470191db0d39e45bd12775c529
stefpi <stef@ceatanostra.org>
2022-10-11 21:26:20 -0400

    about us css

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -26,34 +26,6 @@ function Landing () {
           />
         </div>
       </Margins>
-      /* <Box sx={{ flexGrow: 1, flexDirection: 'row', height: '100vh' }}>
-        <Typography
-          variant="h3"
-          component="div"
-          align="left"
-          sx={{
-            flexGrow: 1,
-            color: 'black',
-            margin: 15,
-            marginTop: 20,
-            display: 'inline-block',
-          }}
-        >
-          Find restaurants together, {<br />}without the hassle
-        </Typography>
-        {/* <img
-          src={fox}
-          alt="fox"
-          loading="lazy"
-          style={{
-            float: 'right',
-            position: 'absolute',
-            top: '10%',
-            right: '10%',
-            marginTop: 20,
-          }}
-        /> */
-      // </Box> */
     )
 }
 
