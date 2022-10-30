60a34944bc984ffbb960b9266f6e5a6deb4bfa2a
Yves <yalikalfic@yahoo.com>
2022-10-30 13:08:22 -0400

    revised landing page

---
+++ src/components/AppSurvey.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+// import '../styles/survey.scss'
+import { Helmet } from 'react-helmet'
+import Margins from './Margins'
+import app from '../assets/images/app.png'
+
+function AppSurvey(props) {
+  return (
+    <Margins>
+      <div className="background" ref={props.innerRef}>
+        <div className="app">
+          <img src={app} className="app-demo" alt="demonstration of app"></img>
+        </div>
+        <div className="right-side">
+          <h1>Subscribe to our Newsletter</h1>
+          <div className="survey-background">
+            <Helmet>
+              <script src="https://tally.so/widgets/embed.js"></script>
+            </Helmet>
+            <div className="survey">
+              <iframe
+                data-tally-src="https://tally.so/embed/3NqrYb?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+                width="100%"
+                height="200"
+                frameborder="0"
+                marginheight="0"
+                marginwidth="0"
+                title="DishWay Waitlist"
+              ></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Margins>
+  )
+}
+
+export default AppSurvey
